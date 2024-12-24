861a9db469d7f6e922e11ea69f9044d04380d146
vatiza <rakibur.mac@gmail.com>
2024-12-24 22:13:56 +0600

    fix missing dependency

--- src/components/ui/typing-animation.tsx
+++ src/components/ui/typing-animation.tsx
@@ -31,13 +31,13 @@ export default function TypingAnimation({
     return () => {
       clearInterval(typingEffect);
     };
-  }, [duration, i]);
+  }, [duration, i, text]);
 
   return (
     <h1
       className={cn(
         "font-display text-center text-4xl font-bold leading-[5rem] tracking-[-0.02em] drop-shadow-sm",
-        className,
+        className
       )}
     >
       {displayedText ? displayedText : text}
